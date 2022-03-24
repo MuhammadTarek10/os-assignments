aa45b6ab6577d86c6f3bfc854fdcbe69e1589c0e
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-03-24 11:14:56 +0200

    finished assignment

--- Assignment-1/main.c
+++ Assignment-1/main.c
@@ -1,22 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-#define SIZE 100
-
-// char* getCommands
+#include "shell/shell.h"
 
 int main(){
-    int pid = fork();
-    char input[SIZE];
-    if (pid == 0){
-        printf("Child\n");
-        gets(input);
-    }
-    else{
-        wait(NULL);
-        printf("Parent\n");
-    }
+    getJobDone();
     return 0;
 }
