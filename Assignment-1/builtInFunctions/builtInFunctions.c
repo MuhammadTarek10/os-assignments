06c215717347ae18b8aabbbbeaaff00601a76510
Muhammad Tarek <muhammadtarek@Muhammads-MacBook-Air.local>
2022-03-25 10:11:43 +0200

    modified error message

--- Assignment-1/builtInFunctions/builtInFunctions.c
+++ Assignment-1/builtInFunctions/builtInFunctions.c
@@ -1,7 +1,7 @@
 
 int checkCD(char** args){
     if(args[1] == NULL)
-        fprintf(stderr, "Expected command cd...\n");
+        fprintf(stderr, "Expected command after cd...\n");
     else{
         if(chdir(args[1]) != 0)
             perror("Error in Changing Directory...\n");
