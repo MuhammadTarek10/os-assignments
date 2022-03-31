c1cd49b169d71ba07609b814a5bef0c2c3ccd9ff
taghreed-taha <72798806+taghreed-taha@users.noreply.github.com>
2022-03-31 22:59:10 +0200

    Add files via upload

--- Assignment-1/builtInFunctions/builtInFunctions.c
+++ Assignment-1/builtInFunctions/builtInFunctions.c
@@ -1,5 +1,5 @@
-
 bool checkCD(char** args){
+    //dealing with cd
     if(args[1] == NULL)
         fprintf(stderr, "Expected command after cd...\n");
     else{
@@ -10,10 +10,18 @@ bool checkCD(char** args){
     return true;
 }
 
-bool checkExit(char** args){
+bool checkHelp(char** args){//dealing with help
+    printf("***welcom to help***\nlist of commands supported:\n>cd\n>ls\n>exit\n>all other general commands available in UNIX shell\n>improper space handling\n");
+    return true;
+}
+
+bool checkExit(char** args){//dealing with exit
     return false;
 }
 
 int getBuiltInNums(){
     return sizeof(builtInChar) / sizeof(char*);
 }
+
+
+
